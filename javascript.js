b8570eed09c45a5f495e8aa542e01c1901cabd9a
MuralidharanPM <mugilmugilone@gmail.com>
2024-08-26 20:24:10 +0530

    rock paper scissor revision v01 commit

--- javascript.js
+++ javascript.js
@@ -15,19 +15,32 @@ function rollStart(callState) {
     var newParagraph = document.createElement("p");
     newParagraph.id = "resultLoc";
     var textNode = "";
+    let isFailureCase = false;
+    if (compuOpt == "Rock" && callState == "Paper") {
+        isFailureCase = true;
+    } else if (compuOpt == "Paper" && callState == "Scissor") {
+        isFailureCase = true;
+    } else if (compuOpt == "Scissor" && callState == "Rock") {
+        isFailureCase = true;
+    }
     if (compuOpt == callState) {
-        (newParagraph.classList).add("winContCls");
-        textNode = document.createTextNode("You won this round");
-    } else{
-        (newParagraph.classList).add("lostContCls");
-        if (randomNum == 1) {
-            (newParagraph.classList).add("rockContCls");
-        } else if (randomNum == 2) {
-            (newParagraph.classList).add("paperContCls");
+        (newParagraph.classList).add("drawContCls");
+        textNode = document.createTextNode("It is a Draw");
+    } else {
+        if (isFailureCase) {
+            (newParagraph.classList).add("winContCls");
+            textNode = document.createTextNode("You won this round");
         } else {
-            (newParagraph.classList).add("scissorContCls");
+            (newParagraph.classList).add("lostContCls");
+            if (randomNum == 1) {
+                (newParagraph.classList).add("rockContCls");
+            } else if (randomNum == 2) {
+                (newParagraph.classList).add("paperContCls");
+            } else {
+                (newParagraph.classList).add("scissorContCls");
+            }
+            textNode = document.createTextNode("You lost this round to");
         }
-        textNode = document.createTextNode("You lost this round to");
     }
     newParagraph.appendChild(textNode);
     document.getElementById("resultSection").appendChild(newParagraph);
